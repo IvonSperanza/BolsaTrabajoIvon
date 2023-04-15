529ff469963d81250508389330f94ab48fb030f1
Ivon Speranza <speranzaivon@gmail.com>
2023-04-15 10:51:52 -0300

    Add project files.

---
+++ BolsaTrabajoIvon/Class2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolsaTrabajoIvon
+{
+    internal class BolsaTrabajo
+    {
+        private string _sector;
+        private string _nombre;
+        private int _experiencia;
+        private int _sueldo;
+
+        public int Experiencia
+        {
+            get { return _experiencia; }
+            set { _experiencia = value; }
+        }
+
+        public int Sueldo
+        {
+            get { return _sueldo; }
+            set { _sueldo = value; }
+        }
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public string Sector
+        {
+            get { return _sector; }
+            set { _sector = value; }
+        }
+
+        public string Incremento(int monto)
+        {
+            if (_sueldo <= monto)
+            {
+                _sueldo += monto;
+                //Console.WriteLine("Su monto final es {_saldo}");
+
+            }
+            Console.WriteLine(_sueldo);
+            return ".";
+        }
+
+
+        public string Decremento(int monto)
+        {
+            if (_sueldo >= monto)
+            {
+                _sueldo -= monto;
+
+
+            }
+            //Console.WriteLine("Su monto final es {_saldo}");
+            Console.WriteLine(_sueldo);
+            return ".";
+        }
+        }
+}
---
+++ BolsaTrabajoIvon/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolsaTrabajoIvon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+      
+                BolsaTrabajo mitrabajo = new BolsaTrabajo();
+
+                Console.WriteLine("Ingrese su nombre:");
+                string _nombre = Console.ReadLine();
+                mitrabajo.Nombre = _nombre;
+
+
+                Console.WriteLine("Ingrese el sector donde desea trabajar: ");
+                string _sector = Console.ReadLine();
+                mitrabajo.Sector = _sector;
+
+                Console.WriteLine("Ingrese sus años de experiencia:");
+                string respuestaE = Console.ReadLine();
+                int _experiencia = int.Parse(respuestaE);
+                mitrabajo.Experiencia = _experiencia;
+
+
+                Console.WriteLine("Ingrese su remuneración esperada:");
+                string respuestaS = Console.ReadLine();
+                int _sueldo = int.Parse(respuestaS);
+                mitrabajo.Sueldo = _sueldo;
+
+                Console.WriteLine("Desea Incrementar o Decrementar su remuneracion, seleccione I o D");
+                string opcion = Console.ReadLine();
+
+                if (opcion == "D")
+                {
+                    Console.WriteLine("Ingrese el monto: ");
+                    string respuestaEX = Console.ReadLine();
+                    int monto = int.Parse(respuestaEX);
+
+                    string sueldo = mitrabajo.Decremento(monto);
+                    Console.WriteLine(sueldo);
+                }
+                
+               else if (opcion == "I") 
+                    {
+                    Console.WriteLine("Ingrese el monto: ");
+                    string respuestaIncr = Console.ReadLine();
+                    int monto = int.Parse(respuestaIncr);
+
+                string sueldo = mitrabajo.Incremento(monto);
+                Console.WriteLine(sueldo);
+            }
+
+            }
+
+        }
+    }
+  
